c81ec21b5626fb82d0b40fd7b518dba4bbdf33cc
zehndouze <thanyaphisit_s@cmu.ac.th>
2026-03-27 21:45:38 +0700

    Update social link to Facebook in Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,14 +6,14 @@ const Footer: React.FC = () => {
     <footer className="footer-section">
       <div className="footer-content">
         <img src="/footer_logo.png" alt="Flipflop Express" className="footer-logo-img" />
-        
+
         <div className="social-links">
-          <a href="#twitter" className="social-link">Twitter/X</a>
+          <a href="https://www.facebook.com/profile.php?id=61575480176806" target="_blank" rel="noopener noreferrer" className="social-link">Facebook Page</a>
           <a href="#discord" className="social-link">Discord</a>
           <a href="#youtube" className="social-link">YouTube</a>
           <a href="https://drive.google.com/drive/folders/1UD6jIUM4VGmwq7rkkAElxqLvOzjS3vF3?usp=sharing" target="_blank" rel="noopener noreferrer" className="social-link">Press Kit</a>
         </div>
-        
+
         <div className="legal-section">
           <p className="copyright">&copy; {new Date().getFullYear()} Flipflop Express Studio. All rights reserved.</p>
           <p className="disclaimer">"Do Knot Lie" is a trademark of Flipflop Express Studio.</p>
